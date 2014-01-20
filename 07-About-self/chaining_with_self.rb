9cd851968a6e8bfef25dafa547abb164bee0fbe6
Denver <denver@denver.mobile.lan>
2014-01-20 16:30:27 +0100

    Solution to the self challenge

--- 07-About-self/chaining_with_self.rb
+++ 07-About-self/chaining_with_self.rb
@@ -1,25 +1,29 @@
 class Animal
   # Modify the following methods to enable chaining !
-  
+
   def name(name)
     @name = name
+    self
   end
   
   def specie(specie)
     @specie = specie
+    self
   end
   
   def color(color)
     @color = color
+    self
   end
   
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
+    self
   end
-  
 end
 
 # Testing chaining
+
 fox = Animal.new.name("Fox").specie("mammals").color("red")
 fox.natural_habitat("forest")
 puts fox.inspect
